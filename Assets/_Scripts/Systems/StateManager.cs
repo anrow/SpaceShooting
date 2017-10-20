299f88d59036f5d4c904d56aba1b483cc1526261
anrow <anrow>
2017-10-20 16:16:29 +0900

    StateManager Update

--- Assets/_Scripts/Systems/StateManager.cs
+++ Assets/_Scripts/Systems/StateManager.cs
@@ -1,33 +1,65 @@
-﻿namespace StateManagement {
+﻿using System;
+using System.Collections.Generic;
+
+namespace StateManagement {
     
     public class StateManager<T> {
 
-        public State<T> curentState{ get; private set; }
-        public T state;
+		public event Action OnStateChanged;
 
-        public StateManager( T _state ) {
-            state = _state;
-            curentState = null;
+		private readonly Dictionary<Type, State<T>> states = new Dictionary<Type, State<T>>( );
+
+		private State<T> currentState;
+
+		public StateManager( State<T> _initalState ) {
+			_initalState.SetStateManager( this );
+			states.Add( _initalState.GetType( ), _initalState );
+			currentState = _initalState;
+			_initalState.EnterState( );
         }
 
-        public void SetState( State<T> _newState ) {
-            if( curentState != null ) {
-                curentState.ExitState( state );
-            }
-            curentState = _newState;
-            curentState.EnterState( state );
-        }
+		public void UpdateState( ) {
+			if( currentState != null ) {
+				currentState.UpdateState( );
+			}
+		}
 
-        public void Update( ) {
-            if( curentState != null ) {
-                curentState.UpdateState( state );
-            } 
-        }
-    }
+		public void SetState<TR>( ) where TR : State<T> {
+			
+			var newState = typeof( TR );
 
-    public abstract class State<T> {
-        public abstract void EnterState( T _state );
-        public abstract void UpdateState( T _state );
-		public abstract void ExitState (T _state);
-    }
+			if( currentState.GetType( ) == newState ) {
+				return;
+			}
+
+			if( currentState != null ) {
+				currentState.ExitState( );
+			}
+
+			currentState = GetState( newState );
+			currentState.EnterState( );
+
+			if( OnStateChanged != null ) {
+				OnStateChanged( );
+			}
+
+		}
+
+		private State<T> GetState( Type _newState ) {
+			if( states.ContainsKey( _newState ) ) {
+				return states[ _newState ];
+			}
+
+			var state = Activator.CreateInstance( _newState ) as State<T>;
+
+			if( state != null ) {
+				state.SetStateManager( this );
+				states.Add( _newState, state );
+				return state;
+			}
+			return null;
+		}
+	}
+
+ 
 }
---
+++ Assets/_Scripts/Systems/TestMain.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StateManagement;
+
+public class TestMain : MonoBehaviour {
+	
+	private StateManager<TestBase> stateManager;
+
+	void Start( ){
+		stateManager = new StateManager<TestBase>( new Test1() );
+	}
+
+	void Update( ) {
+		stateManager.UpdateState( );
+	}
+}
